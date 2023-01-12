06bd91b7f41b650df3b67c4d1a8cebeb5b532ae8
Tony Zhang <zhgtony.pro@hotmail.com>
2023-01-12 22:43:13 +0100

    pokedex

---
+++ js/pokedex.js
@@ -0,0 +1,31 @@
+let url = "https://pokeapi.co/api/v2/pokemon?limit=151"
+
+async function getPokemon(url) {
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+}
+
+async function printPokemon() {
+    const response = await fetch(url)
+    const data = await response.json()
+    const pokemons = data.results
+    let html = ''
+    await Promise.all(pokemons.map(async function(pokemon) {
+        const url = pokemon.url
+        const pokeData = await getPokemon(url)
+        html +=
+            `
+            <div class="card">
+            <img src="${pokeData.sprites.front_default}" alt="${pokeData.name}">
+                <h2>${pokeData.name}</h2>
+                <span>${pokeData.height}</span>
+                <span>${pokeData.weight}</span>
+                <div>${pokeData.types[0].type.name}</div>
+            </div>
+            `
+    }))
+    document.getElementById('pokemonCard').innerHTML = html
+}
+
+printPokemon()
