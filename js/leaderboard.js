f4558655d67a8a3f6f64ab1de777d1278ff71f9d
Rowars2 <romain.parisot@edu.devinci.fr>
2023-01-13 16:08:49 +0100

    fix

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -24,3 +24,54 @@ fetch(`https://api.airtable.com/v0/app1m0A2sG5NrkwN9/tblbumoNEm0DncWNV`, {
         let users = users.sort((a, b) => a - b)
     });
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let draggableElement = document.getElementsByClassName("ball");
+
+// Ajoutez un écouteur d'événement pour le début de la glissière
+draggableElement.addEventListener("mousedown", function(event) {
+  // Enregistrez les coordonnées de départ de la souris
+    var startX = event.clientX;
+    var startY = event.clientY;
+
+  // Ajoutez un écouteur d'événement pour le déplacement de la souris
+    document.addEventListener("mousemove", drag);
+
+  // Ajoutez un écouteur d'événement pour la fin de la glissière
+    document.addEventListener("mouseup", stopDrag);
+
+  // Définir la fonction de glissière
+    function drag(event) {
+        // Calculer la distance de déplacement de la souris
+        let deltaX = event.clientX - startX;
+        let deltaY = event.clientY - startY;
+
+        // Appliquez ces déplacements aux styles de positionnement de l'élément glissant
+        draggableElement.style.left = deltaX + "px";
+        draggableElement.style.top = deltaY + "px";
+    }
+
+  // Définir la fonction pour arrêter la glissière
+    function stopDrag() {
+    // Supprimez les écouteurs d'événement pour le déplacement de la souris et la fin de la glissière
+    document.removeEventListener("mousemove", drag);
+    document.removeEventListener("mouseup", stopDrag);
+  }
+});
